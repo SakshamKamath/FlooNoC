29e277727a0cee1a782caf27eac18060f0e8e80f
Gianluca Bellocchi <gbellocchi95@unimore.it>
2023-11-20 11:59:36 +0100

    Add license header to GeNoc components

--- util/genoc/genoc.py
+++ util/genoc/genoc.py
@@ -1,10 +1,37 @@
-#!/usr/bin/env python3
+'''
+    =====================================================================
 
-# Copyright 2023 University of Modena and Reggio Emilia.
-# Licensed under the Apache License, Version 2.0, see LICENSE for details.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Author: Gianluca Bellocchi <gianluca.bellocchi@unimore.it>
+    Copyright (C) 2023 University of Modena and Reggio Emilia
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    =====================================================================
+
+    Project:        GeNoC
+
+    Title:          Cluster Generation
+
+    Description:    This script specializes and generates a FlooNoc.
+
+    Date:           9.10.2023
+
+    Author: 		Gianluca Bellocchi <gianluca.bellocchi@unimore.it>
+
+    =====================================================================
+
+'''
+
+#!/usr/bin/env python3
 
 import sys
 from mako.template import Template
@@ -32,9 +59,9 @@ target='xilzu9eg'
 topology = 1
 
 # NoC topology
-n_tiles = 256
-n_tiles_x = 16
-n_tiles_y = 16
+n_tiles = 4
+n_tiles_x = 2
+n_tiles_y = 2
 
 # System
 n_clusters = n_tiles
