25fa6d48283d382471782b762985d6e00fe56852
Saksham Kamath <65229373+SakshamKamath@users.noreply.github.com>
2026-07-09 13:05:34 +0200

    Added floo_axi_chimney_wrap with NOC-Splitter and Splitter-NI interface

--- hw/floo_axi_chimney_wrap.sv
+++ hw/floo_axi_chimney_wrap.sv
@@ -92,68 +92,80 @@ module floo_axi_chimney_wrap
   input  floo_rsp_t floo_rsp_i
 );
 
-// Internal Bus declaration
-floo_req_t floo_req_mstreq;
-floo_rsp_t floo_rsp_mstrsp;
+// // Internal Bus declaration
+  floo_req_t floo_req_i_ni_splitter;
+  floo_rsp_t floo_rsp_o_ni_splitter;
+  floo_req_t floo_req_o_ni_splitter;
+  floo_rsp_t floo_rsp_i_ni_splitter;
+
+  floo_req_t floo_req_i_router_splitter;
+  floo_rsp_t floo_rsp_o_router_splitter;
+  floo_req_t floo_req_o_router_splitter;
+  floo_rsp_t floo_rsp_i_router_splitter;
+
+  floo_axi_chimney  #(
+    .AxiCfg(AxiCfg),
+    .ChimneyCfg(set_ports(ChimneyDefaultCfg, 1'b1, 1'b1)),
+    .RouteCfg(RouteCfg),
+    .id_t(id_t),
+    .rob_idx_t(rob_idx_t),
+    .hdr_t  (hdr_t),
+    .sam_rule_t(sam_rule_t),
+    .Sam(Sam),
+    .axi_in_req_t(axi_in_req_t),
+    .axi_in_rsp_t(axi_in_rsp_t),
+    .axi_out_req_t(axi_out_req_t),
+    .axi_out_rsp_t(axi_out_rsp_t),
+    .floo_req_t(floo_req_t),
+    .floo_rsp_t(floo_rsp_t)
+  ) i_axi_chimney (
+    .clk_i,
+    .rst_ni,
+    .test_enable_i,
+    .sram_cfg_i,
+    .axi_in_req_i,
+    .axi_in_rsp_o,
+    .axi_out_req_o,
+    .axi_out_rsp_i,
+    .id_i,
+    .route_table_i,
+    .floo_req_o     (floo_req_i_ni_splitter),
+    .floo_rsp_i     (floo_rsp_o_ni_splitter),
+    .floo_req_i     (floo_req_o_ni_splitter),
+    .floo_rsp_o     (floo_rsp_i_ni_splitter)
+  );
 
  floo_burst_splitter #(
     .AxiCfg(AxiCfg),
-    .MaxReadTxns  ( 4 ), //DOUBT
-    .MaxWriteTxns ( 4 ), //DOUBT
+    .MaxReadTxns  ( 4 ), 
+    .MaxWriteTxns ( 4 ), 
     .FullBW       ( 1'b1              ),
-    .AddrWidth    ( AxiCfg.AddrWidth ), //DOUBT
-    .DataWidth    ( AxiCfg.DataWidth ), //DOUBT
-    .IdWidth      ( AxiCfg.InIdWidth ), //DOUBT
-    .UserWidth    ( AxiCfg.UserWidth ), //DOUBT
+    .AddrWidth    ( AxiCfg.AddrWidth ), 
+    .DataWidth    ( AxiCfg.DataWidth ), 
+    .IdWidth      ( AxiCfg.InIdWidth ), 
+    .UserWidth    ( AxiCfg.UserWidth ), 
     .floo_req_t   ( floo_req_t        ),
     .floo_rsp_t   ( floo_rsp_t        ),
     .hdr_t        ( hdr_t             )
   ) i_floo_burst_splitter (
     .clk_i,
     .rst_ni,
-    .slv_req_i    (floo_req_mstreq),
-    .slv_resp_o   (floo_rsp_mstrsp),
-    .mst_req_o    (floo_req_o),
-    .mst_resp_i   (floo_rsp_i)
+    // interface to noc ni
+    .floo_req_i_ni_splitter,
+    .floo_rsp_o_ni_splitter,
+    .floo_req_o_ni_splitter,
+    .floo_rsp_i_ni_splitter,
+    // interface to noc router
+    .floo_req_i_router_splitter,
+    .floo_rsp_o_router_splitter,
+    .floo_req_o_router_splitter,
+    .floo_rsp_i_router_splitter
   ); 
 
-
-
-
-
-floo_axi_chimney  #(
-  .AxiCfg(AxiCfg),
-  .ChimneyCfg(set_ports(ChimneyDefaultCfg, 1'b1, 1'b1)),
-  .RouteCfg(RouteCfg),
-  .id_t(id_t),
-  .rob_idx_t(rob_idx_t),
-  .hdr_t  (hdr_t),
-  .sam_rule_t(sam_rule_t),
-  .Sam(Sam),
-  .axi_in_req_t(axi_in_req_t),
-  .axi_in_rsp_t(axi_in_rsp_t),
-  .axi_out_req_t(axi_out_req_t),
-  .axi_out_rsp_t(axi_out_rsp_t),
-  .floo_req_t(floo_req_t),
-  .floo_rsp_t(floo_rsp_t)
-) i_axi_chimney (
-  .clk_i,
-  .rst_ni,
-  .test_enable_i,
-  .sram_cfg_i,
-  .axi_in_req_i,
-  .axi_in_rsp_o,
-  .axi_out_req_o,
-  .axi_out_rsp_i,
-  .id_i,
-  .route_table_i,
-  .floo_req_o     (floo_req_mstreq),
-  .floo_rsp_i     (floo_rsp_mstrsp),
-  .floo_req_i,
-  .floo_rsp_o
-);
-
-
+  assign floo_req_i_router_splitter = floo_req_i;
+  assign floo_rsp_i_router_splitter = floo_rsp_i;
+  assign floo_req_o = floo_req_o_router_splitter;
+  assign floo_rsp_o = floo_rsp_o_router_splitter;
 
 endmodule
 
